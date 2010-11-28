39a0cfa306f8c0a93c82b7ffb9831bc67ee38870
Jon Reid <jon.reid@mac.com>
2010-11-28 21:15:36 +0000

    Added support for Python deployment
    git-svn-id: https://hamcrest.googlecode.com/svn/trunk/hamcrest-python@633 2aa0e9f1-8e24-0410-adc4-1b6a3a6e65e2

--- hamcrest-unit-test/__init__.py
+++ hamcrest-unit-test/__init__.py
@@ -3,6 +3,7 @@ __copyright__ = "Copyright 2010 www.hamcrest.org"
 __license__ = "BSD, see License.txt"
 __version__ = "1.0"
 
-from core.matcher_assert import assert_that
-from core import *
-from library import *
+from alltests import alltests
+
+def additional_tests():
+    return alltests()
---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = 'Hamcrest',
+    version = '1.0',
+    author = 'Jon Reid',
+    maintainer_email = 'hamcrest-dev@googlegroups.com',
+    description = 'Hamcrest framework for matcher objects',
+    license = 'BSD',
+    url = 'http://code.google.com/p/hamcrest/',
+    packages = find_packages(),
+    test_suite = 'hamcrest-unit-test.alltests',
+    provides = ['hamcrest',],
+    long_description=read('README'),
+    classifiers = [
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.6',
+        'Topic :: Software Development',
+        'Topic :: Software Development :: Quality Assurance',
+        'Topic :: Software Development :: Testing',
+        ],
+    )
