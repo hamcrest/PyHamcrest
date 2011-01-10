113cbf1301c25c165241996fe1389920f18d11da
Jeong-Min Lee <falsetru@gmail.com>
2011-01-11 00:21:47 +0900

    Revert "Split hamcrest/version.py out of hamcrest/__init__.py; Remove dirty hack from setup.py" Doesn't work.
    This reverts commit 7c432b92181c53465ae02524192e7d627805e428.

--- hamcrest/__init__.py
+++ hamcrest/__init__.py
@@ -1,7 +1,7 @@
+__version__ = "1.2.1"
 __author__ = "Jon Reid"
 __copyright__ = "Copyright 2011 hamcrest.org"
 __license__ = "BSD, see License.txt"
-from version import __version__
 
 from core.matcher_assert import assert_that
 from core import *
