c6951ecef975ccfde066ed2df5b26d023fa8f79b
Jon Reid <jon.reid@mac.com>
2011-01-23 15:42:44 +0800

    Improved "less_than_or_equal_to" description.

--- hamcrest-unit-test/number/ordering_comparison_test.py
+++ hamcrest-unit-test/number/ordering_comparison_test.py
@@ -7,47 +7,64 @@ if __name__ == '__main__':
     sys.path.insert(0, '..')
     sys.path.insert(0, '../..')
 
-import unittest
+from hamcrest.library.number.ordering_comparison import *
 
-from hamcrest.core.assert_that import assert_that
-from hamcrest.core.core.isnot import is_not
-from hamcrest.library.number.ordering_comparison import greater_than
-from hamcrest.library.number.ordering_comparison import greater_than_or_equal_to
-from hamcrest.library.number.ordering_comparison import less_than
-from hamcrest.library.number.ordering_comparison import less_than_or_equal_to
-
+from datetime import date
 from matcher_test import MatcherTest
 
 
 class OrderingComparisonTest(MatcherTest):
 
     def testComparesObjectsForGreaterThan(self):
-        assert_that(2, greater_than(1))
-        assert_that(1, is_not(greater_than(1)))
+        self.assert_matches('match', greater_than(1), 2)
+        self.assert_does_not_match('no match', greater_than(1), 1)
 
     def testComparesObjectsForLessThan(self):
-        assert_that(0, less_than(1))
-        assert_that(1, is_not(less_than(1)))
+        self.assert_matches('match', less_than(1), 0)
+        self.assert_does_not_match('no match', less_than(1), 1)
 
     def testComparesObjectsForGreaterThanOrEqualTo(self):
-        assert_that(2, greater_than_or_equal_to(1))
-        assert_that(1, greater_than_or_equal_to(1))
-        assert_that(0, is_not(greater_than_or_equal_to(1)))
+        self.assert_matches('match', greater_than_or_equal_to(1), 2)
+        self.assert_matches('match', greater_than_or_equal_to(1), 1)
+        self.assert_does_not_match('no match', greater_than_or_equal_to(1), 0)
 
     def testComparesObjectsForLessThanOrEqualTo(self):
-        assert_that(0, less_than_or_equal_to(1))
-        assert_that(1, less_than_or_equal_to(1))
-        assert_that(2, is_not(less_than_or_equal_to(1)))
+        self.assert_matches('match', less_than_or_equal_to(1), 0)
+        self.assert_matches('match', less_than_or_equal_to(1), 1)
+        self.assert_does_not_match('no match', less_than_or_equal_to(1), 2)
+
+    def testSupportsDifferentTypesOfComparableObjects(self):
+        self.assert_matches('strings', greater_than('bb'), 'cc')
+        self.assert_matches('dates', less_than(date.today()), date.min)
 
     def testHasAReadableDescription(self):
         self.assert_description('a value greater than <1>', greater_than(1))
-        # Following test reveals typo in java hamcrest:
         self.assert_description('a value greater than or equal to <1>',
                                 greater_than_or_equal_to(1))
         self.assert_description('a value less than <1>', less_than(1))
-        # Reveals awkward wording that should be fixed:
-        self.assert_description('a value equal to or less than <1>',
+        self.assert_description('a value less than or equal to <1>',
                                 less_than_or_equal_to(1))
+
+    def testSuccessfulMatchDoesNotGenerateMismatchDescription(self):
+        self.assert_no_mismatch_description(greater_than(1), 2)
+        self.assert_no_mismatch_description(less_than(1), 0)
+        self.assert_no_mismatch_description(greater_than_or_equal_to(1), 1)
+        self.assert_no_mismatch_description(less_than_or_equal_to(1), 1)
+
+    def testMismatchDescription(self):
+        self.assert_mismatch_description("was <0>", greater_than(1), 0)
+        self.assert_mismatch_description("was <2>", less_than(1), 2)
+        self.assert_mismatch_description("was <0>",
+                                         greater_than_or_equal_to(1), 0)
+        self.assert_mismatch_description("was <2>",
+                                         less_than_or_equal_to(1), 2)
+
+    def testDescribeMismatch(self):
+        self.assert_describe_mismatch("was <0>", greater_than(1), 0)
+        self.assert_describe_mismatch("was <2>", less_than(1), 2)
+        self.assert_describe_mismatch("was <0>",
+                                      greater_than_or_equal_to(1), 0)
+        self.assert_describe_mismatch("was <2>", less_than_or_equal_to(1), 2)
 
 
 if __name__ == '__main__':
