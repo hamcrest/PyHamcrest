a4ed3fdca40876f82b3e0270543d1b0873cbf92d
Chris Rose <offby1@offby1.net>
2011-04-02 12:16:44 -0600

    Trove classifier: 3.2

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ params = dict(
         'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.1',
+        'Programming Language :: Python :: 3.2',
         'Topic :: Software Development',
         'Topic :: Software Development :: Quality Assurance',
         'Topic :: Software Development :: Testing',
