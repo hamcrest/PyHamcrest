69ceb6d31276694a6ca395c6f2f2cecffdb0c864
Jon Reid <jon.reid@mac.com>
2011-09-27 20:25:48 -0700

    Bumping version to 1.6

--- hamcrest/__init__.py
+++ hamcrest/__init__.py
@@ -1,7 +1,7 @@
 from core import *
 from library import *
 
-__version__ = "1.5"
+__version__ = "1.6"
 __author__ = "Jon Reid"
 __copyright__ = "Copyright 2011 hamcrest.org"
 __license__ = "BSD, see License.txt"
