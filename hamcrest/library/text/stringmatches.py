fe4a6d34b0f301b5e4bc6cf23884556cfd9eeb99
Jon Reid <jon.reid@mac.com>
2011-12-26 14:27:43 -0800

    Document matches_regexp

--- hamcrest/library/text/stringmatches.py
+++ hamcrest/library/text/stringmatches.py
@@ -22,6 +22,16 @@ class StringMatchesPattern(BaseMatcher):
 
 
 def matches_regexp(pattern):
+    """Matches if object is a string containing a match for a given regular
+    expression.
+
+    :param pattern: The regular expression to search for.
+
+    This matcher first checks whether the evaluated object is a string. If so,
+    it checks if the regular expression ``pattern`` matches anywhere within the
+    evaluated object.
+
+    """
     if isinstance(pattern, basestring):
         pattern = re.compile(pattern)
 
