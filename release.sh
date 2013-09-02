ab80072def0cfdf1629d84f9537fee03a968dbc9
Chris Rose <offby1@offby1.net>
2013-09-02 10:32:24 -0700

    Deprecate 3.1, add 3.3

--- release.sh
+++ release.sh
@@ -12,24 +12,40 @@ function do_release() {
     return $rc
 }
 
-for python_version in py25 py27 py31 py26 py32; do
-    do_release $python_version || failed="$failed $python_version"
-done
-do_release pypy pypy || failed="$failed pypy"
-# do_release jython jython || failed="$failed jython"
+function do_test() {
+    local rc
+    local ver=$1; shift
+    local python=${1:-python}; shift
+    rm -rf $HERE/build
+    tox -e $ver
+    rc=$?
+    return $rc
+}
 
-rc=0
+function do_all_vers() {
+    for python_version in py25 py26 py27 py32 py33; do
+        $1 $python_version || failed="$failed $python_version"
+    done
+    $1 pypy pypy || failed="$failed pypy"
+    # $1 jython jython || failed="$failed jython"
 
-for f in $failed; do
-    rc=1
-    echo "Failed to release $f"
-done
+    rc=0
 
-if [ $rc -ne 0 ]; then
-    echo "Skipping the source release due to failures"
-    exit $rc
-fi
+    for f in $failed; do
+        rc=1
+        echo "$1 failed for $f"
+    done
 
+    if [ $rc -ne 0 ]; then
+        echo "Skipping $1 due to failures"
+        return $rc
+    fi
+}
+
+do_all_vers do_test || exit 1
+do_all_vers do_release || exit 1
+
+echo "All releases uploaded; uploading source release as well"
 $HERE/.tox/py27/bin/python $HERE/setup.py sdist upload
 
 exit $rc
