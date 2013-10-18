79c8f324da921a7ec178ca11d53e2918630ad0fa
Chris Rose <offby1@offby1.net>
2013-10-18 00:02:06 -0700

    Bump to 1.8.0

--- src/hamcrest/__init__.py
+++ src/hamcrest/__init__.py
@@ -1,7 +1,7 @@
 from core import *
 from library import *
 
-__version__ = "1.8.0-alpha"
+__version__ = "1.8.0"
 __author__ = "Chris Rose"
 __copyright__ = "Copyright 2013 hamcrest.org"
 __license__ = "BSD, see License.txt"
