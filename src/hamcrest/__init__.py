c86efe7d1598cc9f7afc82c3118ec4cc20483312
Chris Rose <offby1@offby1.net>
2014-10-06 08:33:11 -0700

    Revert "Version move to 2.0.0a1"
    This reverts commit 4cd7d3062037dab13c99f0dd9b44f08fbd894ffa.

--- src/hamcrest/__init__.py
+++ src/hamcrest/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from hamcrest.core import *
 from hamcrest.library import *
 
-__version__ = "2.0.0a1"
+__version__ = "1.8.0"
 __author__ = "Chris Rose"
-__copyright__ = "Copyright 2014 hamcrest.org"
+__copyright__ = "Copyright 2013 hamcrest.org"
 __license__ = "BSD, see License.txt"
