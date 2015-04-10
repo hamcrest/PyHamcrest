9db5e37eae16201df4ef7337bf0ecbfced9a4dcb
Chris Rose <offby1@offby1.net>
2015-04-10 14:13:51 -0400

    Version 1.8.3

--- src/hamcrest/__init__.py
+++ src/hamcrest/__init__.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 from hamcrest.core import *
 from hamcrest.library import *
 
-__version__ = "1.8.2"
+__version__ = "1.8.3"
 __author__ = "Chris Rose"
 __copyright__ = "Copyright 2013 hamcrest.org"
 __license__ = "BSD, see License.txt"
