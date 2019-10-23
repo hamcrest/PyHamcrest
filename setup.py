1473ee521c216244ada449e8e78d1b5734b19934
Simon Brunning <simon@brunningonline.net>
2019-10-23 14:11:48 +0100

    Python 3.8 supported.

--- setup.py
+++ setup.py
@@ -53,6 +53,7 @@ params = dict(
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: Jython",
         "Programming Language :: Python :: Implementation :: PyPy",
