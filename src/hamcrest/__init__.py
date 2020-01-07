dc4c523ee3bb6e00a44b7ab5fdd57d4c4b2163c0
Simon Brunning <simon@brunningonline.net>
2020-01-07 12:50:13 +0000

    Set version to 1.10 for release, and update copyright year.

--- src/hamcrest/__init__.py
+++ src/hamcrest/__init__.py
@@ -1,7 +1,7 @@
 from hamcrest.core import *
 from hamcrest.library import *
 
-__version__ = "2.0.0a1"
+__version__ = "1.10.0"
 __author__ = "Chris Rose"
-__copyright__ = "Copyright 2014 hamcrest.org"
+__copyright__ = "Copyright 2020 hamcrest.org"
 __license__ = "BSD, see License.txt"
