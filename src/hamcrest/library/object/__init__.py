c9262dc145071dacacc623b2b0837a5ca44b00e4
Perry Goy <perry@Perrys-MacBook-Pro.local>
2020-04-01 13:51:34 -0500

    sort imports for isort.

--- src/hamcrest/library/object/__init__.py
+++ src/hamcrest/library/object/__init__.py
@@ -1,8 +1,8 @@
 """Matchers that inspect objects and classes."""
 
-from .hasreturnvalue import has_return_value
 from .haslength import has_length
 from .hasproperty import has_properties, has_property
+from .hasreturnvalue import has_return_value
 from .hasstring import has_string
 
 __author__ = "Jon Reid"
