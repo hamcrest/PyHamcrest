ba5ab7f5b3725bdbdfab210c054f5bc2b7317772
Chris Rose <offline@offby1.net>
2020-05-01 07:13:13 -0700

    Blacken the examples

--- examples/CustomDateMatcher.py
+++ examples/CustomDateMatcher.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append('..')
+
+sys.path.append("..")
 
 from hamcrest.core.base_matcher import BaseMatcher
 from hamcrest.core.helpers.hasmethod import hasmethod
@@ -17,16 +18,22 @@ class IsGivenDayOfWeek(BaseMatcher):
 
     def _matches(self, item):
         """Test whether item matches."""
-        if not hasmethod(item, 'weekday'):
+        if not hasmethod(item, "weekday"):
             return False
         return item.weekday() == self.day
 
     def describe_to(self, description):
         """Describe the matcher."""
-        day_as_string = ['Monday', 'Tuesday', 'Wednesday', 'Thursday',
-                         'Friday', 'Saturday', 'Sunday']
-        description.append_text('calendar date falling on ')    \
-                   .append_text(day_as_string[self.day])
+        day_as_string = [
+            "Monday",
+            "Tuesday",
+            "Wednesday",
+            "Thursday",
+            "Friday",
+            "Saturday",
+            "Sunday",
+        ]
+        description.append_text("calendar date falling on ").append_text(day_as_string[self.day])
 
 
 def on_a_saturday():
@@ -37,19 +44,19 @@ def on_a_saturday():
 class SampleTest(unittest.TestCase):
     def testDateIsOnASaturday(self):
         """Example of successful match."""
-        d = datetime.date(2008, 04, 26)
+        d = datetime.date(2008, 4, 26)
         assert_that(d, is_(on_a_saturday()))
 
     def testFailsWithMismatchedDate(self):
         """Example of what happens with date that doesn't match."""
-        d = datetime.date(2008, 04, 06)
+        d = datetime.date(2008, 4, 6)
         assert_that(d, is_(on_a_saturday()))
 
     def testFailsWithNonDate(self):
         """Example of what happens with object that isn't a date."""
-        d = 'oops'
+        d = "oops"
         assert_that(d, is_(on_a_saturday()))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
--- examples/ExampleWithAssertThat.py
+++ examples/ExampleWithAssertThat.py
@@ -1,5 +1,6 @@
 import sys
-sys.path.append('..')
+
+sys.path.append("..")
 
 from hamcrest import *
 import unittest
@@ -7,16 +8,16 @@ import unittest
 
 class ExampleWithAssertThat(unittest.TestCase):
     def testUsingAssertThat(self):
-        assert_that('xx', is_('xx'))
-        assert_that('yy', is_not('xx'))
-        assert_that('i like cheese', contains_string('cheese'))
+        assert_that("xx", is_("xx"))
+        assert_that("yy", is_not("xx"))
+        assert_that("i like cheese", contains_string("cheese"))
 
     def testCanAlsoSupplyDescriptiveReason(self):
-        assert_that('xx', is_('xx'), 'description')
+        assert_that("xx", is_("xx"), "description")
 
     def testCanAlsoAssertPlainBooleans(self):
-        assert_that(True, 'This had better not fail')
+        assert_that(True, "This had better not fail")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
