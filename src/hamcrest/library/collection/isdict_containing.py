bbd147d2df4160e5bcae955bc9617700dc1de72b
Simon Brunning <simon@brunningonline.net>
2021-02-24 14:17:12 +0000

    Fix for https://github.com/hamcrest/PyHamcrest/issues/156 - if has_entry() has only a single matching key, report the mismatching value.

--- src/hamcrest/library/collection/isdict_containing.py
+++ src/hamcrest/library/collection/isdict_containing.py
@@ -1,4 +1,4 @@
-from typing import Hashable, Mapping, TypeVar, Union
+from typing import Hashable, Mapping, MutableMapping, TypeVar, Union
 
 from hamcrest.core.base_matcher import BaseMatcher
 from hamcrest.core.description import Description
@@ -31,6 +31,24 @@ class IsDictContaining(BaseMatcher[Mapping[K, V]]):
         description.append_text("a dictionary containing [").append_description_of(
             self.key_matcher
         ).append_text(": ").append_description_of(self.value_matcher).append_text("]")
+
+    def describe_mismatch(self, item: Mapping[K, V], mismatch_description: Description) -> None:
+        key_matches: MutableMapping[K, V] = {}
+        if hasmethod(item, "items"):
+            for key, value in item.items():
+                if self.key_matcher.matches(key):
+                    key_matches[key] = value
+        if len(key_matches) == 1:
+            key, value = key_matches.popitem()
+            mismatch_description.append_text("value for ").append_description_of(key).append_text(
+                " "
+            )
+            self.value_matcher.describe_mismatch(value, mismatch_description)
+        else:
+            super().describe_mismatch(item, mismatch_description)
+
+    def describe_match(self, item: Mapping[K, V], match_description: Description) -> None:
+        super().describe_match(item, match_description)
 
 
 def has_entry(
