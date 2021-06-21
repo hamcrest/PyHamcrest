a93a67a4139114c818c14b3c022fb6861efa1bef
Felix Hammerl <felix.hammerl@gmail.com>
2021-06-21 19:27:25 +0200

    Add __all__ to core
    Missing __all__ causes Pylance claim that assert_that has not been imported for wildcard imports.
    
    Fixes #175

--- src/hamcrest/core/__init__.py
+++ src/hamcrest/core/__init__.py
@@ -4,3 +4,21 @@ from hamcrest.core.core import *
 __author__ = "Jon Reid"
 __copyright__ = "Copyright 2011 hamcrest.org"
 __license__ = "BSD, see License.txt"
+
+__all__ = [
+    "assert_that",
+    "all_of",
+    "any_of",
+    "described_as",
+    "is_",
+    "anything",
+    "equal_to",
+    "instance_of",
+    "none",
+    "not_none",
+    "is_not",
+    "not_",
+    "same_instance",
+    "calling",
+    "raises"
+]
