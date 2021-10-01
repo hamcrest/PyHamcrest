d20c7cf7d5c1d3e7eabac39aad4817b459e99b6c
Denis Alexeev <github.com@alexeev.ws>
2021-10-02 00:22:12 +0300

    add a couple of tests

--- tests/hamcrest_unit_test/base_matcher_test.py
+++ tests/hamcrest_unit_test/base_matcher_test.py
@@ -37,6 +37,19 @@ class BaseMatcherTest(unittest.TestCase):
     def testMatchDescriptionShouldDescribeItem(self):
         assert_match_description("was <99>", PassingBaseMatcher(), 99)
 
+    def testMatcherReprShouldDescribeMatcher(self):
+        assert repr(FailingBaseMatcher()) == "<FailingBaseMatcher(SOME DESCRIPTION)>"
+
+    def testMatcherReprShouldTruncateLongDescription(self):
+        class LongDescriptionMatcher(BaseMatcher):
+            def describe_to(self, description):
+                description.append_text("1234 " * 13)
+
+        assert (
+            repr(LongDescriptionMatcher())
+            == "<LongDescriptionMatcher(1234 1234 1234 1234 1234 1234 1234 1234 1234 1234 1234...)>"
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
