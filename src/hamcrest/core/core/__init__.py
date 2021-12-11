7b4178a8aba38fb312f8f748762c95ed87c45392
Serg Tereshchenko <serg.partizan@gmail.com>
2021-12-11 17:14:52 +0200

    fix(typing): Added __all__ to package root
    Refs #175

--- src/hamcrest/core/core/__init__.py
+++ src/hamcrest/core/core/__init__.py
@@ -15,3 +15,20 @@ from hamcrest.core.core.raises import calling, raises
 __author__ = "Jon Reid"
 __copyright__ = "Copyright 2011 hamcrest.org"
 __license__ = "BSD, see License.txt"
+
+__all__ = [
+    "all_of",
+    "any_of",
+    "anything",
+    "calling",
+    "described_as",
+    "equal_to",
+    "instance_of",
+    "is_",
+    "is_not",
+    "none",
+    "not_",
+    "not_none",
+    "raises",
+    "same_instance",
+]
