9c81a386f26119554d2135949f34d644e322cab4
Chris Rose <offline@offby1.net>
2022-08-06 10:16:52 -0700

    add hatch-vcs for versioning

--- src/hamcrest/__init__.py
+++ src/hamcrest/__init__.py
@@ -1,8 +1,9 @@
 from hamcrest.core import *
 from hamcrest.library import *
 from hamcrest import core, library
+from hamcrest._version import version
 
-__version__ = "2.0.4"
+__version__ = version
 __author__ = "Chris Rose"
 __copyright__ = "Copyright 2020 hamcrest.org"
 __license__ = "BSD, see License.txt"
