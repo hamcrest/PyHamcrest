f90b396994126219612d7593d96325ba7c39c632
Chris Rose <offline@offby1.net>
2022-08-07 08:54:48 -0700

    dev: made the release script use hatch/towncrier

--- release.sh
+++ release.sh
@@ -1,51 +1,22 @@
 #!/usr/bin/env bash
-HERE=$(unset CDPATH; cd `dirname $0`; pwd)
-cd $HERE
+set -eu -o pipefail
+HERE="$(
+    unset CDPATH
+    cd "$(dirname "$0")"
+    pwd
+)"
+cd "$HERE"
 
-function do_release() {
-    local rc
-    local ver=$1; shift
-    local python=${1:-python}; shift
-    rm -rf $HERE/build
-    $HERE/.tox/$ver/bin/$python $HERE/setup.py build bdist_egg upload
-    rc=$?
-    return $rc
-}
+HATCH_CURRENT=$(hatch version)
+TAG=${1:?"a tag must be provided. Consider using hatch's next: V$HATCH_CURRENT"}
 
-function do_test() {
-    local rc
-    local ver=$1; shift
-    local python=${1:-python}; shift
-    rm -rf $HERE/build
-    tox -e $ver
-    rc=$?
-    return $rc
-}
+# towncrier needs the version to be tagged before running
+git tag "$TAG"
 
-function do_all_vers() {
-    for python_version in py35 py36 py37 py38; do
-        $1 $python_version || failed="$failed $python_version"
-    done
-    $1 pypy pypy || failed="$failed pypy"
-    # $1 jython jython || failed="$failed jython"
+# generate the changelog
+hatch run towncrier build --yes
 
-    rc=0
+# re-tag
+git tag -f "$TAG"
 
-    for f in $failed; do
-        rc=1
-        echo "$1 failed for $f"
-    done
-
-    if [ $rc -ne 0 ]; then
-        echo "Skipping $1 due to failures"
-        return $rc
-    fi
-}
-
-do_all_vers do_test || exit 1
-do_all_vers do_release || exit 1
-
-echo "All releases uploaded; uploading source release as well"
-$HERE/.tox/py37/bin/python $HERE/setup.py sdist upload
-
-exit $rc
+echo "To release, run 'git push origin --tags \"$TAG\"'"
